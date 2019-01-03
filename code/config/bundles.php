772e9446aa595ddd0eca2834e8d8f50317750e50
Adrian Chlubek <adrian.chlubek@thesoftwarehouse.pl>
2019-01-03 10:00:50 +0100

    Initial commit with websockets bundle installed

---
+++ code/config/bundles.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Gos\Bundle\PubSubRouterBundle\GosPubSubRouterBundle::class => ['all' => true],
+    Gos\Bundle\WebSocketBundle\GosWebSocketBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+];
